3cd5154a6a573c8a51e59008033fd5970c8971db
WDominic <dom0_o@protonmail.com>
2022-12-15 21:58:24 -0500

    simple actix server setup

--- server_agent/src/main.rs
+++ server_agent/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, Responder, Result, HttpResponse, HttpServer};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyObj {
+    name: String,
+}
+
+#[get("/auth")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("data")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{App, HttpServer};
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 1337))?
+        .run()
+        .await
 }
